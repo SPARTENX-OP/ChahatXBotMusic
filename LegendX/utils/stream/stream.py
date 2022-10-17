217f5b2d8cb265d4613a6cffe1535734a3f6b9cb
SPARTANBOY xD <114400345+SPARTENX-OP@users.noreply.github.com>
2022-10-17 05:47:45 -0700

    Update stream.py

--- LegendX/utils/stream/stream.py
+++ LegendX/utils/stream/stream.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup
 
 import config
 from LegendX import Carbon, YouTube, app
-from LegendX.core.call import Anon
+from LegendX.core.call import Legend
 from LegendX.misc import db
 from LegendX.utils.database import (add_active_chat,
                                        add_active_video_chat,
@@ -16,7 +16,7 @@ from LegendX.utils.exceptions import AssistantErr
 from LegendX.utils.inline.play import (stream_markup,
                                           telegram_markup)
 from LegendX.utils.inline.playlist import close_markup
-from LegendX.utils.pastebin import Anonbin
+from LegendX.utils.pastebin import Legendbin
 from LegendX.utils.stream.queue import put_queue, put_queue_index
 from LegendX.utils.thumbnails import gen_thumb
 
@@ -40,7 +40,7 @@ async def stream(
         if not await is_video_allowed(chat_id):
             raise AssistantErr(_["play_7"])
     if forceplay:
-        await Anon.force_stop_stream(chat_id)
+        await Legend.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['playlist_16']}\n\n"
         count = 0
@@ -89,7 +89,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_16"])
-                await Anon.join_call(
+                await Legend.join_call(
                     chat_id, original_chat_id, file_path, video=status
                 )
                 await put_queue(
@@ -120,7 +120,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await Anonbin(msg)
+            link = await Legendbin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -170,7 +170,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anon.join_call(
+            await Legend.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -224,7 +224,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anon.join_call(
+            await Legend.join_call(
                 chat_id, original_chat_id, file_path, video=None
             )
             await put_queue(
@@ -278,7 +278,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anon.join_call(
+            await Legend.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -339,7 +339,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Anon.join_call(
+            await Legend.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anon.join_call(
+            await Legend.join_call(
                 chat_id,
                 original_chat_id,
                 link,
