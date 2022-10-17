1a8bd99486afd7b1a05ba69cd73d6d582acd7b7e
SPARTANBOY xD <114400345+SPARTENX-OP@users.noreply.github.com>
2022-10-17 10:48:01 -0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -22,7 +22,7 @@ SONG_DOWNLOAD_DURATION = int(
     getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180")
 )
 
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001835443285"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001890174278"))
 
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "🥀🎼𓆩 ≛⎯⳰͢⃝🥀ᴹⁱˢˢ★Cђ͢͢aђaτ★🥀🕊️⃝🎼🥀")
 
