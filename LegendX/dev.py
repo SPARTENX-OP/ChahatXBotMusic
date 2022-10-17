e83e536f61421d0edfd7a31777e9f45c9232b342
SPARTANBOY xD <114400345+SPARTENX-OP@users.noreply.github.com>
2022-10-17 00:28:43 -0700

    Create dev.py

---
+++ LegendX/dev.py
@@ -0,0 +1,14 @@
+from legendx modules import * 
+clients profiles.pro
+        if 
+        legendpro=x{event}
+                      event(0)
+                            legend(deqsys).utils
+                                     utils.loading.sever
+                                         def plugins import dev.py($py)
+                            pytgcalls(install.("pip install pytgcalls")
+                            pyrogram(install.(:"pip install pyrogram , telethon ,telegramClients")
+                                    loading-server(program:base if legend-server(import*{in}))/
+                                     close
+                                     class:10 loop
+                                     
